164772498ebcd1a5382c212cce997e91122a58c2
kaickreis <kaick.cardoso.reis@gmail.com>
2025-09-04 23:57:20 +0000

    apagar exesso e definir tam#

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,7 +3,7 @@
 #define TAM 5  // tamanho do tabuleiro (5x5 para exemplo)
 
 int main() {
-    int tabuleiro[TAM][TAM] = {0};
+    int tabuleiro[5][5] = {0};
 
     // --- Definição manual das posições ---
     // Navio horizontal (linha 2, colunas 1 a 3)
@@ -38,34 +38,6 @@ int main() {
         }
         printf("\n");
     }
-
-    return 0;
-}
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
